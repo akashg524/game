e42b3bb1f86e8d182547f86d1761017e69c50b5d
akashg524 <akashg524@gmail.com>
2018-10-04 11:40:36 +0530

    make it better

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 const express= require('express'),
 path = require('path');
-const app =express();
 
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
-server.use(middlewares);
-server.use(router);
-server.listen(port);
 app.use(express.static('./dist/game'));
 
 app.get('/*', (req,res)=>{
