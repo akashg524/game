735d3648e5c28f8647d0b3c466b2b3281c1a9f87
akashg524 <akashg524@gmail.com>
2018-10-05 13:44:13 +0530

    make it better

--- server.js
+++ server.js
@@ -2,15 +2,7 @@ const express= require('express'),
 path = require('path');
 const app =express();
 app.use(express.static('./dist/game'));
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
 
-server.use(middlewares);
-server.use(router);
-server.listen(port);
 app.get('/*', (req,res)=>{
     
 res.sendFile(path.join(__dirname,'/dist/game/index.html'));
